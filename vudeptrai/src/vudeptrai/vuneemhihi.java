127b9d6f594554e99a4850bba63a0a034f3951d8
Đinh Huỳnh Vũ <153527266+dinhvune12@users.noreply.github.com>
2025-05-16 10:22:35 +0700

    update lan 2

---
+++ vudeptrai/src/vudeptrai/vuneemhihi.java
@@ -0,0 +1,91 @@
+package vudeptrai;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class vuneemhihi {
+
+	private JFrame frame;
+	private JTextField soB;
+	private JTextField soA;
+	private JLabel lblSB;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vuneemhihi window = new vuneemhihi();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public vuneemhihi() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1084, 504);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		soB = new JTextField();
+		soB.setBounds(342, 81, 96, 19);
+		frame.getContentPane().add(soB);
+		soB.setColumns(10);
+		
+		soA = new JTextField();
+		soA.setBounds(342, 141, 96, 19);
+		frame.getContentPane().add(soA);
+		soA.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Số A");
+		lblNewLabel.setEnabled(false);
+		lblNewLabel.setBounds(170, 72, 88, 36);
+		frame.getContentPane().add(lblNewLabel);
+		
+		lblSB = new JLabel("Số B");
+		lblSB.setEnabled(false);
+		lblSB.setBounds(170, 124, 88, 36);
+		frame.getContentPane().add(lblSB);
+		
+		JButton cong = new JButton("cong");
+		cong.setBounds(76, 259, 85, 21);
+		frame.getContentPane().add(cong);
+		
+		JButton nhan = new JButton("nhân");
+		nhan.setBounds(440, 259, 85, 21);
+		frame.getContentPane().add(nhan);
+		
+		JButton tru = new JButton("trừ");
+		tru.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		tru.setBounds(255, 259, 85, 21);
+		frame.getContentPane().add(tru);
+		
+		JButton chia = new JButton("chia");
+		chia.setBounds(637, 259, 85, 21);
+		frame.getContentPane().add(chia);
+	}
+}
